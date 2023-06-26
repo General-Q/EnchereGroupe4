668eeda3aa73867f1154c9ad5d2791c46bba2608
qmasson2023 <qmasson2023@12SE22-J0MRZS2.ad.campus-eni.fr>
2023-06-26 11:38:09 +0200

    création de la page html et des packages bo et controller

---
+++ Enchere/src/main/java/fr/eni/enchere/controller/utilisateurController.java
@@ -0,0 +1,12 @@
+package fr.eni.enchere.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class utilisateurController {
+
+	public utilisateurController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
