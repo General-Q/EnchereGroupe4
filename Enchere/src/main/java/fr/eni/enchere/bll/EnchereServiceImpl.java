1221177076291dcbf5bebec0f1cfe9f384e58764
schevallier2023 <schevallier2023@12SE22-D2MRZS2.ad.campus-eni.fr>
2023-06-26 17:25:30 +0200

    modif fin de journée sur EnchereService et EnchereServiceImpl et controller

--- Enchere/src/main/java/fr/eni/enchere/bll/EnchereServiceImpl.java
+++ Enchere/src/main/java/fr/eni/enchere/bll/EnchereServiceImpl.java
@@ -1,5 +1,23 @@
 package fr.eni.enchere.bll;
 
+import fr.eni.enchere.bo.Enchere;
+import fr.eni.enchere.dal.EnchereDAO;
+import jakarta.validation.Valid;
+
 public class EnchereServiceImpl implements EnchereService{
+
+	private EnchereDAO enchereDAO;
+	
+	@Override
+	public Enchere findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void ajouterVente(@Valid Enchere enchere) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
