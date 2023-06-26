9cb13ae548af8a144c5904b2ce46d776a4d3fc3d
qmasson2023 <qmasson2023@12SE22-J0MRZS2.ad.campus-eni.fr>
2023-06-26 16:11:09 +0200

    accueil & identification html + déclaration List<Enchere> controller

--- Enchere/src/main/java/fr/eni/enchere/controller/utilisateurController.java
+++ Enchere/src/main/java/fr/eni/enchere/controller/utilisateurController.java
@@ -1,9 +1,27 @@
 package fr.eni.enchere.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import fr.eni.enchere.bo.Enchere;
 
 @Controller
 public class utilisateurController {
+	
+	@GetMapping({"/", "/accueil"})
+	public String afficherEncheres(Model model){
+		List<Enchere> encheres = enchereService.consulterEncheres();
+		return "accueil";
+	}
+	
+	@GetMapping("/identification")
+	public String identUser() {
+		return "identification";
+	}
+	
 
 	public utilisateurController() {
 		// TODO Auto-generated constructor stub
