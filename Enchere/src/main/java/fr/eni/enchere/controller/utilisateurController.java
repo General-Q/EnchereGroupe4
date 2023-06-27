38aac97f3f9f12f13d5fea4c669354487f866201
qmasson2023 <qmasson2023@12SE22-J0MRZS2.ad.campus-eni.fr>
2023-06-27 14:13:59 +0200

    Commit h1 Test

--- Enchere/src/main/java/fr/eni/enchere/controller/utilisateurController.java
+++ Enchere/src/main/java/fr/eni/enchere/controller/utilisateurController.java
@@ -1,10 +1,6 @@
 package fr.eni.enchere.controller;
 
- 
-
 import java.util.List;
-
- 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,74 +10,59 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
- 
-
 import fr.eni.enchere.bll.ArticleVenduService;
 import fr.eni.enchere.bll.EnchereService;
 import fr.eni.enchere.bo.ArticleVendu;
 import fr.eni.enchere.bo.Enchere;
 import jakarta.validation.Valid;
 
- 
-
 @Controller
 public class utilisateurController {
 
-    private ArticleVenduService articleVenduService;
+	private ArticleVenduService articleVenduService;
 
-    @Autowired
-    public utilisateurController(ArticleVenduService articleVenduService) {
-        this.articleVenduService = articleVenduService;
-    }
+	@Autowired
+	public utilisateurController(ArticleVenduService articleVenduService) {
+		this.articleVenduService = articleVenduService;
+	}
 
-    @GetMapping({"/", "/accueil"})
-    public String afficherEncheres(Model model){
-        List<ArticleVendu> articlesVendus = articleVenduService.consulterAV();
+	@GetMapping({ "/", "/accueil" })
+	public String afficherEncheres(Model model) {
+		List<ArticleVendu> articlesVendus = articleVenduService.consulterAV();
 
-         if (articlesVendus == null) {
-                // Gérer le cas où la liste est nulle, par exemple, afficher un message d'erreur
-                model.addAttribute("message", "Aucun article vendu disponible.");
-            } else {
-                // Ajouter la liste des articles vendus au modèle pour l'affichage dans la vue
-                model.addAttribute("articlesVendus", articlesVendus);
-            }
+		if (articlesVendus == null) {
+			// Gérer le cas où la liste est nulle, par exemple, afficher un message d'erreur
+			model.addAttribute("message", "Aucun article vendu disponible.");
+		} else {
+			// Ajouter la liste des articles vendus au modèle pour l'affichage dans la vue
+			model.addAttribute("articlesVendus", articlesVendus);
+		}
 
- 
+		return "accueil";
+	}
 
-        return "accueil";
-    }
+	@GetMapping("/identification")
+	public String identUser() {
+		return "identification";
+	}
+	/*
+	 * @GetMapping("/detail_vente") public String detailVente(Integer id, Model
+	 * model) { Enchere enchere = enchereService.findById(id);
+	 * model.addAttribute("enchere", enchere); return "detail_vente";
+	 * 
+	 * }
+	 */
 
+	@PostMapping("/nouvelle_vente")
+	public String ajouterVente(@Valid @ModelAttribute Enchere enchere, BindingResult validationResult) {
+		if (validationResult.hasErrors()) {
+			return "nouvelle_vente";
 
- 
+		}
+		// enchereService.ajouterVente(enchere);
 
-    @GetMapping("/identification")
-    public String identUser() {
-        return "identification";
-    }
-    /*
-    @GetMapping("/detail_vente")
-    public String detailVente(Integer id, Model model) {
-        Enchere enchere = enchereService.findById(id);
-        model.addAttribute("enchere", enchere);
-        return "detail_vente";
+		return "redirection:/accueil";
 
-    }*/
-
-
-    @PostMapping("/nouvelle_vente")
-    public String ajouterVente(@Valid @ModelAttribute Enchere enchere, BindingResult validationResult) { 
-        if (validationResult.hasErrors()) {
-            return "nouvelle_vente";
-
-        }
-    //enchereService.ajouterVente(enchere);
-
-    return "redirection:/accueil";
-
-
-    }
-
- 
-
+	}
 
 }
--- Enchere/src/main/java/fr/eni/enchere/dal/ArticleVenduDAOImpl.java
+++ Enchere/src/main/java/fr/eni/enchere/dal/ArticleVenduDAOImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import fr.eni.enchere.bo.ArticleVendu;
 @Repository
 public class ArticleVenduDAOImpl implements ArticleVenduDAO{
-	private final String FIND_ALL = "SELECT no_article, nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente";
+	private final String FIND_ALL = "SELECT no_article, nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente FROM ARTICLES_VENDUS";
 
 	@Autowired
 	private NamedParameterJdbcTemplate jdbcTemplate;
