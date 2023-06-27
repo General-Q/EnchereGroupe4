0fc9a2d94ba2191b07f309611d37b47895ff50fd
dvisse2023 <dvisse2023@12SE22-DSLRZS2.ad.campus-eni.fr>
2023-06-27 11:57:00 +0200

    correction erreurs

--- Enchere/src/main/java/fr/eni/enchere/bll/EnchereServiceImpl.java
+++ Enchere/src/main/java/fr/eni/enchere/bll/EnchereServiceImpl.java
@@ -8,7 +8,7 @@ import jakarta.validation.Valid;
 public class EnchereServiceImpl implements EnchereService{
 
 	private EnchereDAO enchereDAO;
-	
+	/*
 	@Override
 	public Enchere findById(Integer id) {
 	Enchere enchere = null;
@@ -19,11 +19,16 @@ public class EnchereServiceImpl implements EnchereService{
 	}
 		return enchere;
 	}
-
+*/
 	@Override
 	public void ajouterVente(@Valid Enchere enchere) {
 		
 		
+	}
+	@Override
+	public Enchere findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- Enchere/src/main/java/fr/eni/enchere/dal/UtilisateurDAOImpl.java
+++ Enchere/src/main/java/fr/eni/enchere/dal/UtilisateurDAOImpl.java
@@ -1,14 +1,9 @@
 package fr.eni.enchere.dal;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
-
-@Repository
-public class UtilisateurDAOImpl {
-=======
 import java.sql.ResultSet;
 import java.sql.SQLException;
->>>>>>> branch 'master' of https://github.com/General-Q/EnchereGroupe4.git
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
@@ -49,7 +44,7 @@ public class UtilisateurDAOImpl implements UtilisateurDAO{
 			user.setNom(rs.getString("nom"));
 			user.setPrenom(rs.getString("prenom"));
 			user.setEmail(rs.getString("email"));
-			user.setAdminsitrateur(rs.getBoolean("administrateur"));
+			user.setAdministrateur(rs.getBoolean("administrateur"));
 			return user;
 		}
 		
